419fee1fea49eb217ff00d9c7ba59763fe372561
Ross Mongrue <rossmongrue@Rosss-MacBook-Pro.local>
2026-03-29 08:37:08 -0500

    Remove pages config causing redirect loop

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -10,9 +10,6 @@ export const authOptions: NextAuthOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
-  pages: {
-    signIn: '/api/auth/signin',
-  },
   secret: process.env.NEXTAUTH_SECRET,
   session: {
     strategy: 'jwt',
